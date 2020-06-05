a43b57e84fded1c81ffa0679b5227b694ecc5824
Jason Clegg <tjason.clegg@gmail.com>
2020-06-05 10:22:32 -0600

    Have the first 2 MVP requirements complete. Starting on testing.

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -4,6 +4,14 @@ import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    const { getByText } = render(<CheckoutForm />)
 
-test("form shows success message on submit with form details", () => {});
+    const header = getByText(/Checkout Form/i)
+
+    expect(header).toBeInTheDocument();
+});
+
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm />)
+});
